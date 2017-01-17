0896ae714dc363c46a15be5317b1ff440f68b715
Danish Kamal <danish8802204230@gmail.com>
2017-01-17 13:09:27 +0530

    Cron Examples Added

--- imports/ui/itemTemplate.js
+++ imports/ui/itemTemplate.js
@@ -10,10 +10,3 @@ Template.itemTemplate.helpers({
         return Product.find({});
     },
 });
-
-/*Template.hello.events({
-    'click button'(event, instance) {
-        // increment the counter when button is clicked
-        instance.counter.set(instance.counter.get() + 1);
-    },
-});*/
